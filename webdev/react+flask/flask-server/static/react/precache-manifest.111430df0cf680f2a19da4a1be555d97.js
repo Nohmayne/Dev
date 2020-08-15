fc3b691f29e42b3bc29e6ada081de68028070c30
Caden Henrich <cadenhenrich@gmail.com>
2020-08-15 12:11:20 -0400

    Linked flask-server and react-app through proxy in package.json

---
+++ webdev/react+flask/flask-server/static/react/precache-manifest.111430df0cf680f2a19da4a1be555d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8aa384e4e7234835cc25e97cf326b24",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "bdca50fbc6295feb2135",
+    "url": "/static/react/css/main.25e61275.chunk.css"
+  },
+  {
+    "revision": "a0036068cfe4df099cce",
+    "url": "/static/react/js/2.1524d4e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.1524d4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdca50fbc6295feb2135",
+    "url": "/static/react/js/main.f71d37fe.chunk.js"
+  },
+  {
+    "revision": "00b90c6979e7b9785281",
+    "url": "/static/react/js/runtime-main.3a44c33a.js"
+  }
+]);
